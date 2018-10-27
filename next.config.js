63bbd429e0d5feaf3cc63974a65cb30d6eab1f9d
Jeff Escalante <gh.je@mailhero.io>
2018-10-26 18:41:58 -0700

    global styles loading scheme

--- next.config.js
+++ next.config.js
@@ -1,26 +1,49 @@
 const path = require('path')
 
 module.exports = {
+  exportPathMap: function() {
+    return { '/': { page: '/' } }
+  },
   webpack: config => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: 'emit-file-loader',
-          options: {
-            name: 'dist/[path][name].[ext].js'
+    config.module.rules.push(
+      {
+        test: /(?<!(global))\.css$/,
+        use: [
+          {
+            loader: 'emit-file-loader',
+            options: { name: 'dist/[path][name].[ext].js' }
+          },
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              extends: path.resolve(__dirname, './babel.config.js')
+            }
+          },
+          { loader: 'styled-jsx-css-loader' }
+        ]
+      },
+      {
+        test: /(?:global)\.css$/,
+        use: [
+          {
+            loader: 'emit-file-loader',
+            options: { name: 'dist/[path][name].[ext].js' }
+          },
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              extends: path.resolve(__dirname, './babel.config.js')
+            }
+          },
+          {
+            loader: 'styled-jsx-css-loader',
+            options: { global: true }
           }
-        },
-        {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            extends: path.resolve(__dirname, './babel.config.js')
-          }
-        },
-        'styled-jsx-css-loader'
-      ]
-    })
+        ]
+      }
+    )
 
     return config
   }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import globalBase from '../components/global-styles/base.global.css'
 import Button from '../components/button'
 
 const Index = () => (
   <div>
+    <style jsx>{globalBase}</style>
     <h3>HashiCorp next.js text</h3>
     <Button title="Testing" url="#" theme="dark-outline" />
   </div>
