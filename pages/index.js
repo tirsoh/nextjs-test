afa34c57d5f7e07f66c417f71c87659a064a8e9e
Jeff Escalante <gh.je@mailhero.io>
2018-10-26 18:50:29 -0700

    more global styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import globalBase from '../components/global-styles/base.global.css'
+import typeBase from '../components/global-styles/typography.global.css'
+import codeHighlightBase from '../components/global-styles/code-highlighting.global.css'
 import Button from '../components/button'
 
 const Index = () => (
   <div>
     <style jsx>{globalBase}</style>
+    <style jsx>{typeBase}</style>
+    <style jsx>{codeHighlightBase}</style>
     <h3>HashiCorp next.js text</h3>
     <Button title="Testing" url="#" theme="dark-outline" />
   </div>
