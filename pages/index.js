ca75533b5f5580b810efd26ac6b26e14da3ae0d7
Mike Wickett <mike@wickett.ca>
2018-10-28 13:28:16 -0400

    Add image component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import globalBase from '../components/global-styles/base.global.css'
 import typeBase from '../components/global-styles/typography.global.css'
 import codeHighlightBase from '../components/global-styles/code-highlighting.global.css'
+
 import Button from '../components/button'
 import SectionHeader from '../components/section-header'
+import Image from '../components/image'
+import CaseStudySlider from '../components/case-study-slider'
+import CaseStudySliderData from '../components/case-study-slider/data'
 
 const Index = () => (
   <div>
@@ -27,8 +31,27 @@ const Index = () => (
       </li>
       <li>
         <h6>Section header</h6>
-        <SectionHeader headline="This is a headline" description="now for a really long description of some text that you might never read" use_h1={true} />
-        </li>
+        <SectionHeader
+          headline="This is a headline"
+          description="now for a really long description of some text that you might never read"
+          use_h1={true}
+        />
+      </li>
+      <li>
+        <h6>Image</h6>
+        <p>Picture element</p>
+        <Image
+          src="https://www.datocms-assets.com/2885/1538142087-ye-endahl.jpg"
+          alt="Test image"
+          aspect_ratio="16,9"
+        />
+        <p>SVG</p>
+        <Image
+          src="https://www.datocms-assets.com/2885/1524097005-adobe-black-1.svg"
+          alt="Test SVG image"
+          svg={true}
+        />
+      </li>
     </ul>
   </div>
 )
