6f03240540964e2e66a6291dabf7288a8af9f0ac
Michelle Savage <MSavage07@gsb.columbia.edu>
2018-10-30 19:45:11 -0500

    Initial install of Toggle component onto Nextjs

---
+++ components/toggle/index.js
@@ -0,0 +1,34 @@
+import styles from './style.css'
+import slugify from 'slugify'
+import { Component } from 'react'
+
+export default class Toggle extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        enabled: this.props.enabled || false
+      }
+    }
+  
+    render() {
+      return (
+        <div
+          className={['switch', this.state.enabled ? 'on' : ''].join(' ')}
+          onClick={(() =>
+            this.setState({ enabled: !this.state.enabled })).bind(this)}
+        >
+          <div className='switch-toggle' />
+
+          <style jsx>{styles}</style>
+        </div>
+        
+      )
+    }
+  
+    componentDidUpdate(prevProps) {
+      // if a parent changes the enabled prop value, reflect it to the state
+      if (this.props.enabled !== prevProps.enabled) {
+        this.setState({ enabled: this.props.enabled })
+      }
+    }
+  }
