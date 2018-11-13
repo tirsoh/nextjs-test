e453168f14e8705ef6ab1c16b4388e40fc692696
Jeff Escalante <gh.je@mailhero.io>
2018-11-13 15:38:41 -0500

    button bugfix

--- components/button/index.js
+++ components/button/index.js
@@ -1,7 +1,7 @@
 import css from './style.css'
 import slugify from 'slugify'
 
-export default ({
+export default function Button({
   title,
   url,
   external,
@@ -9,7 +9,7 @@ export default ({
   ga_prefix,
   classes,
   ...additionalProps
-}) => {
+}) {
   if (theme && typeof theme === 'object') theme = theme.slug
   const gaSlug = slugify(title, { lower: true })
   const ext = external === 'true' || external === true
