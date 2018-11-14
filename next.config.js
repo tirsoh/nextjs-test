838af718e324d547bb54a757ed4b8dd8d9236095
Jeff Escalante <gh.je@mailhero.io>
2018-11-14 00:06:22 -0500

    markdown file resolution

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
+const path = require('path')
+const fs = require('fs')
 
 const config = {
   // setup for static export
   exportPathMap: function() {
-    return { '/': { page: '/' } }
+    const root = path.join(__dirname, 'markdown')
+    const output = { '/': { page: '/' } }
+    fs.readdirSync(root).map(file => {
+      output[`/${file.replace(/\.md$/, '')}`] = {
+        page: '/post',
+        asPath: path.join('/markdown', file)
+      }
+    })
+    return output
   },
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
@@ -18,6 +28,14 @@ const config = {
       analyzerMode: 'static',
       reportFilename: '../bundles/client.html'
     }
+  },
+  // setup for markdown file loading
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: ['babel-loader', 'markdown-with-front-matter-loader']
+    })
+    return config
   }
 }
 
