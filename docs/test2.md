e902831605477a93cfa57a1e42ad5fd3e71b00b4
Mike Wickett <mike@wickett.ca>
2018-11-26 12:19:14 -0500

    Revert "Markdown File Loading"

--- docs/test2.md
+++
@@ -1,7 +0,0 @@
----
-page_title: "what a cool test"
----
-
-# Testing Again
-
-This is a **cool** test!
--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
-const path = require('path')
-const fs = require('fs')
 
 const config = {
   // setup for static export
   exportPathMap: function() {
-    const root = path.join(__dirname, 'docs')
-    const output = { '/': { page: '/' } }
-    fs.readdirSync(root).map(file => {
-      output[`/docs/${file.replace(/\.md$/, '')}`] = {
-        page: '/post',
-        asPath: path.join(__dirname, 'docs', file)
-      }
-    })
-    return output
+    return { '/': { page: '/' } }
   },
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
@@ -28,14 +18,6 @@ const config = {
       analyzerMode: 'static',
       reportFilename: '../bundles/client.html'
     }
-  },
-  // setup for markdown file loading
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: ['babel-loader', 'markdown-with-front-matter-loader']
-    })
-    return config
   }
 }
 
