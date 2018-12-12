e0f5aca445f00469d290a1eec5da360943035b56
Michael Schonfeld <michael@schonfeld.org>
2018-12-12 14:34:57 +0100

    add codegen script

---
+++ scripts/apollo-codegen.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Fetching schemas from Dato..."
+apollo-codegen introspect-schema https://graphql.datocms.com --header "Authorization: Bearer 78d2968c99a076419fbb" --output schema.json
+
+echo "Generating Typescript files..."
+apollo-codegen generate ../**/*.graphql --schema schema.json --target typescript --addTypename --output ../types/operation-result-types.ts
+
